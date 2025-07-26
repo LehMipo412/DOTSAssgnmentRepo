4cfc0d29676f247f359bfaa4ee169f8d1d95a1dc
LehMipo412 <barbetzalel412@gmail.com>
2025-07-26 21:41:40 +0300

    fixed bugs in the jobs related scripts

--- Assets/Scripts/CEOOfPrime.cs
+++ Assets/Scripts/CEOOfPrime.cs
@@ -1,24 +1,100 @@
+using Unity.Collections;
+using Unity.Jobs;
 using UnityEngine;
 
 public class CEOOfPrime : MonoBehaviour
 {
     public int executivePrimeSusNum;
-    
-    [ContextMenu ("AssignJobToSus")]
+
+    [ContextMenu("AssignJobToSus")]
     public void AssignJobToSus()
     {
-        var job = new PrimeEmployment { employedSusPrime = executivePrimeSusNum };
+       
+        NativeArray<int> numberToCheck = new NativeArray<int>(1, Allocator.TempJob);
+        NativeArray<bool> isPrimeResult = new NativeArray<bool>(1, Allocator.TempJob);
 
-        job.Execute((int)Mathf.Sqrt(executivePrimeSusNum));
+        
+        numberToCheck[0] = executivePrimeSusNum;
+        isPrimeResult[0] = true; 
 
-        if (!(job.isPrime))
+       
+        int root = (int)Mathf.Ceil(Mathf.Sqrt(executivePrimeSusNum));
+
+      
+        if (executivePrimeSusNum < 2)
         {
-            Debug.Log("this number is not prime ");
+            isPrimeResult[0] = false;
+            Debug.Log($"The number {executivePrimeSusNum} is not prime.");
+           
+            numberToCheck.Dispose();
+            isPrimeResult.Dispose();
+            return;
+        }
+       
+        if (executivePrimeSusNum == 2)
+        {
+            isPrimeResult[0] = true;
+            Debug.Log($"The number {executivePrimeSusNum} is prime.");
+            
+            numberToCheck.Dispose();
+            isPrimeResult.Dispose();
+            return;
+        }
+       
+        if (executivePrimeSusNum % 2 == 0)
+        {
+            isPrimeResult[0] = false;
+            Debug.Log($"The number {executivePrimeSusNum} is not prime.");
+           
+            numberToCheck.Dispose();
+            isPrimeResult.Dispose();
+            return;
+        }
+
+
+        // Create the job instance
+        var job = new PrimeEmployment
+        {
+            Number = numberToCheck,
+            IsPrime = isPrimeResult,
+            MaxIndex = root 
+        };
+
+        // Schedule the job. The length parameter here defines how many times Execute(index) will be called.
+        // We need to iterate from 2 up to 'root'.
+        // Since we already handled even numbers, we can iterate through odd numbers starting from 3.
+        // The `length` parameter for Schedule is the number of iterations.
+        // We'll calculate it such that 'index' in the job represents the odd divisor.
+        // For example, if root is 10, we want to check 3, 5, 7, 9.
+        // The loop in Execute will adjust 'index' to be the actual divisor.
+        // A simple way is to iterate from 1 up to (root - 1) / 2 to cover odd divisors.
+        // Example: 3 -> index 0 (2 * 0 + 3 = 3)
+        //          5 -> index 1 (2 * 1 + 3 = 5)
+        //          7 -> index 2 (2 * 2 + 3 = 7)
+        // The total number of odd divisors to check up to 'root' (excluding 1)
+        // will be roughly (root - 3) / 2 + 1 if root >= 3.
+        // If root < 3, no iterations are needed beyond edge cases already handled.
+        int iterations = (root >= 3) ? (root - 3) / 2 + 1 : 0;
+        if (iterations < 0) iterations = 0; // Ensure non-negative iterations
+
+        // Schedule the job. The batchCount (64) is a hint for how many items to process per job chunk.
+        JobHandle handle = job.ScheduleByRef(iterations, 64);
+
+        // Wait for the job to complete
+        handle.Complete();
+
+        // Read the result from the NativeArray
+        if (!isPrimeResult[0])
+        {
+            Debug.Log($"The number {executivePrimeSusNum} is not prime.");
         }
         else
         {
-            Debug.Log("this number is prime ");
+            Debug.Log($"The number {executivePrimeSusNum} is prime.");
         }
-    }
 
+        // Dispose of the NativeArrays to prevent memory leaks
+        numberToCheck.Dispose();
+        isPrimeResult.Dispose();
+    }
 }
--- Assets/Scripts/PrimeEmployment.cs
+++ Assets/Scripts/PrimeEmployment.cs
@@ -1,16 +1,52 @@
+using Unity.Collections;
 using Unity.Jobs;
-using UnityEngine;
 
-public class PrimeEmployment : IJobParallelFor
+public struct PrimeEmployment : IJobParallelFor
 {
+    
+    [ReadOnly] public NativeArray<int> Number; 
 
-    public int employedSusPrime;
-    public bool isPrime = true;
+    
+    public NativeArray<bool> IsPrime;
+
+    [ReadOnly] public int MaxIndex; 
+
+    // No constructor is needed for jobs when setting members directly from the main thread.
+    // The constructor you had was creating a new NativeArray, which was part of the problem.
+
     public void Execute(int index)
     {
-        if (employedSusPrime%index ==0)
+        // If the number has already been found to be not prime by another thread,
+        // we can early out. This is an optimization.
+        // However, direct reading and writing to IsPrime[0] across threads without an Atomic Safety Handle
+        // or a mutex for a single boolean can be problematic for correctness in highly concurrent scenarios
+        // (though for a simple boolean flag, it often works "enough" for this case).
+        // For strict correctness and larger scale, Interlocked operations or a NativeQueue/NativeHashMap
+        // could be considered to report findings. For primality, simply finding *any* divisor makes it not prime.
+        if (!IsPrime[0])
         {
-            isPrime = false;
+            return;
+        }
+
+        // The 'index' in IJobParallelFor goes from 0 to length-1.
+        // We want to check odd divisors starting from 3.
+        // So, index 0 corresponds to divisor 3, index 1 to divisor 5, etc.
+        // divisor = 2 * index + 3
+        int TempIndex = 2 * index + 3;
+
+        // Only check divisors up to MaxDivisor
+        if (TempIndex > MaxIndex)
+        {
+            return;
+        }
+
+        // Get the number to check from the NativeArray
+        int num = Number[0];
+
+        // If the number is divisible by the current divisor, it's not prime.
+        if (num % TempIndex == 0)
+        {
+            IsPrime[0] = false; // Mark as not prime
         }
     }
 }
